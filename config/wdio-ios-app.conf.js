e269c54f7f72217298c50a5ac580e65caf692717
Adarsh Aravind <adarsharavind@TMAdmins-MacBook-Pro.local>
2023-10-10 11:30:20 +0530

    config update

--- config/wdio-ios-app.conf.js
+++ config/wdio-ios-app.conf.js
@@ -1,13 +1,6 @@
-const { join } = require('path');
 const { config } = require('./wdio-shared.conf');
-
-// ============
-// Specs
-// ============
-config.specs = [
-    './tests/specs/**/app*.spec.js',
-];
-
+const params = require('../tests/.artifacts/parameters.json');
+const data = require(`../${params.configMapping}`).ios;
 // ============
 // Capabilities
 // ============
@@ -21,17 +14,17 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // This is `appium:` for all Appium Capabilities which can be found here
         // http://appium.io/docs/en/writing-running-appium/caps/
-        'appium:deviceName': 'iPhone 11',
-        'appium:platformVersion': '13.4',
+        'appium:deviceName': `${data.deviceName}`,
+        'appium:platformVersion': `${data.platformVersion}`,
         'appium:orientation': 'PORTRAIT',
         // `automationName` will be mandatory, see
         // https://github.com/appium/appium/releases/tag/v1.13.0
         'appium:automationName': 'XCUITest',
         // The path to the app
-        'appium:app': join(process.cwd(), './apps/iOS-Simulator-NativeDemoApp-0.2.1.app.zip'),
+        'appium:app': data.app,
         // Read the reset strategies very well, they differ per platform, see
         // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
-        'appium:noReset': true,
+        'appium:noReset': false,
         'appium:newCommandTimeout': 240,
     },
 ];
